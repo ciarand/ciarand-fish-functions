596da6c940121e9facfa853ee00c1f1ca4852030
Ciaran Downey <me@ciarand.me>
2013-05-20 13:10:34 -0700

    Adds Fish prompt to repo
    This commit adds a default Fish prompt that incorporates the latest exit
    code (via a color coded lightning bolt) on the left, and the current Git
    branch, dirty status (via a color coded circle), and the current working
    directory on the right.

---
+++ fish_prompt.fish
@@ -0,0 +1,15 @@
+function fish_prompt --description 'Write out the prompt'
+    echo (begin;
+        if test $status = 0
+            set_color blue;
+        else
+            set_color red;
+        end;
+    end;
+    begin;
+        echo "⚡";
+    end;
+    begin;
+        set_color normal;
+    end;);
+end

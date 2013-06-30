dbfbc9977a7c918139f6d966a79b4cebda2d4fd5
Ciaran Downey <me@ciarand.me>
2013-06-30 01:27:58 -0700

    Add cleanup calls to the switch_to_lf function
    This fixes an issue were `switch_to_lf` was not correctly cleaning up
    temporary files created during its execution.

--- switch_to_lf.fish
+++ switch_to_lf.fish
@@ -1,12 +1,13 @@
 function switch_to_lf --description 'Fix any problems with line endings by converting the passed files / folders to LF (Unix) line endings'
     for target in $argv;
         echo "Packing $target into $tmpfile";
-        set -l tmpfile (mktemp tmp_archive.zipXXXXXXXXXX).zip;
-        zip -ll -qr0 $tmpfile $target; and begin;
-            echo "Unpacking "$tmpfile" into $target";
-            unzip -aqo $tmpfile; and begin;
-                echo "Cleaning "$tmpfile;
-                rm $tmpfile;
+        set -l tmpfile (mktemp tmp_archive.XXXXXXXXXX)
+        set -l tmpzipfile $tmpfile.zip;
+        zip -ll -qr0 $tmpzipfile $target; and begin;
+            echo "Unpacking "$tmpzipfile" into $target";
+            unzip -aqo $tmpzipfile; and begin;
+                echo "Cleaning "$tmpzipfile;
+                rm $tmpfile $tmpzipfile;
             end;
         end;
     end;

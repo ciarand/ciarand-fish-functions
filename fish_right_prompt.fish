64dbda9f20f0c90f8d76211490cc3a7958cbd204
Ciaran Downey <me@ciarand.me>
2013-07-05 16:28:57 -0700

    Upgrade right_prompt with branches, tags, + paths
    Add new information to the right_prompt, including:
    
    * Git branch information if the branch is not master
    
    * Git tag information, or latest commit hash if there's no associated
      tag, with colored based on the repo's status (red for dirty, blue for
      clean)
    
    * Git-relative pathing information, so when inside a git repo the path
      is displayed relative to the root of the project rather than the file
      system

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,19 +1,73 @@
-# The goal here is to have ⚡ appear on the left,
+# The goal here is to have "❯" appear on the left,
 # and have most of the information appear on the right
 
-function fish_right_prompt --description 'Print the left part of the prompt'
-    set -l git_prompt (begin;
-        # git prompt
-        git rev-parse ^ /dev/null; and begin;
-            set -l current_branch (git rev-parse --abbrev-ref HEAD ^ /dev/null);
-            set -l branch_status (begin;
-                set_color (git diff --quiet --ignore-submodules HEAD ^ /dev/null; and echo blue; or echo red;);
-                echo "●";
-                set_color normal;
-            end;);
-            echo "($current_branch)" "$branch_status";
-        end;
-    end;);
-    set -l current_directory (set_color yellow; echo $PWD | sed -e "s|^$HOME|~|"; set_color normal;);
-    echo "$git_prompt""$current_directory";
+function fish_right_prompt --description 'Print the right part of the prompt'
+    # config settings
+    set -l dirty_color red
+    set -l clean_color blue
+    set -l directory_color yellow
+
+    # local variables
+    set -l true (true; echo $status)
+    # git prompt
+    set -l git_prompt
+    # default dir prompt, may be overwritten by git prompt if applicable
+    set -l dir_prompt (set_color $directory_color; prompt_pwd; set_color normal)
+    # are we in a git repo?
+
+    set -l is_git_repo (git rev-parse --is-inside-work-tree ^ /dev/null > /dev/null; echo $status)
+
+    if test $is_git_repo = $true
+        set -l is_dirty (not git diff --quiet --ignore-submodules HEAD ^ /dev/null; echo $status)
+
+        set -l git_status_color (test $is_dirty = $true; and set_color $dirty_color; or set_color $clean_color);
+        set -l git_normal_color (set_color normal);
+
+        set -l git_prompt (begin
+            # variable declarations
+            set -l git_meta_branch
+            set -l git_meta_tag
+            set -l git_character
+
+            # branch
+            set -l branch (git rev-parse --abbrev-ref HEAD ^ /dev/null)
+            # tag
+            set -l current_tag (git describe --always --tag)
+
+            # character output
+            set git_character (begin
+                echo $git_status_color
+                echo "♆";
+                echo $git_normal_color
+            end)
+
+            # Branch output
+            set git_meta_branch (begin
+                test "$branch" = "master"; or echo "($branch) "
+            end)
+
+            # tag output
+            set git_meta_tag (begin
+                #echo $git_status_color
+                echo "["$git_status_color$current_tag$git_normal_color"]"
+                #echo $git_normal_color
+            end)
+
+            # putting it all together
+            echo "$git_character$git_meta_branch$git_meta_tag"
+        end);
+
+        # now we overwrite the dir_prompt stuff with a git-specific pwd if necessary
+        set -l git_root_path (git rev-parse --show-toplevel)
+        set -l git_root (echo $git_root_path | awk -F'/' '{print $NF}')
+        set -l current_directory_absolute (pwd)
+        set -l current_directory_relative_to_git (echo $current_directory_absolute | sed "s|^"$git_root_path"||")
+
+        set dir_prompt (begin
+            set_color $directory_color
+            echo "$git_root$current_directory_relative_to_git"
+            set_color normal
+        end)
+    end
+    echo "$git_prompt $dir_prompt"
 end

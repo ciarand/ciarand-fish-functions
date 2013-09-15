ddf345e7df8154e8f88d01369a90040e51a1e4b6
Ciaran Downey <me@ciarand.me>
2013-09-14 23:47:31 -0700

    Update search to use `ag` if available
    Ag ("the silver searcher") is an alternative to grep and ack that, apart
    from being 50% and 33% faster to type respectively is significantly
    faster than ack. It also ignores your .gitignore files (and .hgignore if
    that's your thing) unless you tell it otherwise by providing the -U
    flag.
    
    In summary, it's faster and better than ack and grep. Only problem is
    it doesn't come standard, so now this function checks if it's available.
    
    http://geoff.greer.fm/2011/12/27/the-silver-searcher-better-than-ack/

--- fish_functions/search.fish
+++ fish_functions/search.fish
@@ -3,7 +3,14 @@ function search -d 'Quick search in a directory for a string ($argv[1])'
 #
 # Quick search in a directory for a string ($1).
 #
+# Updated by @ciarand on 9/14/13 to use Ag if available.
+# See: http://geoff.greer.fm/2011/12/27/the-silver-searcher-better-than-ack/
+#
 
 # use -iru to search directories ack usually ignores (like .git)
-    ack -i $argv[1]
+    if not type "ag" > /dev/null
+        ag -i $argv
+    else
+        ack -i $argv[1]
+    end
 end
